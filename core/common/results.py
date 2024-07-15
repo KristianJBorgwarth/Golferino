9a4e0a961368e472bb99e7991a5fb75710c2dc7f
Kristian <fich2402@gmail.com>
2024-07-15 20:16:00 +0200

    ref: renamed success bool to is_success in result class

--- core/common/results.py
+++ core/common/results.py
@@ -6,25 +6,25 @@ T = TypeVar('T')
 
 
 class Result(Generic[T]):
-    def __init__(self, success: bool, error: Optional[ErrorMessage] = None, value: Optional[T] = None):
-        self.success = success
+    def __init__(self, is_success: bool, error: Optional[ErrorMessage] = None, value: Optional[T] = None):
+        self.is_success = is_success
         self.error = error
         self._value = value
 
     @property
     def failure(self) -> bool:
-        return not self.success
+        return not self.is_success
 
     @property
     def value(self) -> T:
-        if not self.success:
+        if not self.is_success:
             raise ValueError("Cannot Fetch value from failed result")
         return self._value
 
     @staticmethod
     def ok(value: Optional[T] = None) -> 'Result[T]':
-        return Result(success=True, value=value)
+        return Result(is_success=True, value=value)
 
     @staticmethod
     def fail(error: ErrorMessage) -> 'Result[T]':
-        return Result(success=False, error=error)
+        return Result(is_success=False, error=error)
