b13b32996db4c026d73035e6bcf8ec4894cb4fb5
Lasse Schwenger <lasse.m.schwenger@gmail.com>
2024-07-16 17:58:30 +0200

    removed message and added status_code to ResponseEnvelope

--- core/views/ResponseEnvelope.py
+++ core/views/ResponseEnvelope.py
@@ -5,26 +5,24 @@ from rest_framework import status
 
 
 class ResponseEnvelope:
-    def __init__(self, success, data=None, message=None, error=None, status_code=status.HTTP_200_OK):
+    def __init__(self, success, data=None, error=None, status_code=status.HTTP_200_OK):
         self.success = success
         self.data = data
-        self.message = message
         self.error = error
         self.status_code = status_code
 
     @staticmethod
     def success(data=None, message=None, status_code=status.HTTP_200_OK):
-        return ResponseEnvelope(success=True, data=data, message=message, status_code=status_code)
+        return ResponseEnvelope(success=True, data=data, status_code=status_code)
 
     @staticmethod
     def fail(error, message=None, status_code=status.HTTP_400_BAD_REQUEST):
-        return ResponseEnvelope(success=False, error=error, message=message, status_code=status_code)
+        return ResponseEnvelope(success=False, error=error, status_code=status_code)
 
     def to_response(self):
         response_data = {
             'success': self.success,
             'data': self.data,
-            'message': self.message,
             'error': self.error,
         }
         return Response(response_data, status=self.status_code)
