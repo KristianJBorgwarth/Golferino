3341350ceb5f6e9e347ad52e231975718a83e4e0
Lasse Schwenger <lasse.m.schwenger@gmail.com>
2024-07-16 17:58:41 +0200

    removed message and added status_code to Result class

--- core/common/results.py
+++ core/common/results.py
@@ -6,10 +6,12 @@ T = TypeVar('T')
 
 
 class Result(Generic[T]):
-    def __init__(self, is_success: bool, error: Optional[ErrorMessage] = None, value: Optional[T] = None):
+    def __init__(self, is_success: bool, error: Optional[ErrorMessage] = None, value: Optional[T] = None,
+                 status_code: Optional[int] = None):
         self.is_success = is_success
         self.error = error
         self._value = value
+        self.status_code = status_code
 
     @property
     def failure(self) -> bool:
@@ -22,9 +24,9 @@ class Result(Generic[T]):
         return self._value
 
     @staticmethod
-    def ok(value: Optional[T] = None) -> 'Result[T]':
-        return Result(is_success=True, value=value)
+    def ok(value: Optional[T] = None, status_code: Optional[int] = None) -> 'Result[T]':
+        return Result(is_success=True, value=value, status_code=status_code)
 
     @staticmethod
-    def fail(error: ErrorMessage) -> 'Result[T]':
-        return Result(is_success=False, error=error)
+    def fail(error: ErrorMessage, status_code: Optional[int] = None) -> 'Result[T]':
+        return Result(is_success=False, error=error, status_code=status_code)
