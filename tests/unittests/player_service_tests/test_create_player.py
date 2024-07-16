2f3634d05603ee8851c44b6d241e0e24a6b922c6
Lasse Schwenger <lasse.m.schwenger@gmail.com>
2024-07-17 00:30:59 +0200

    fixed unittests after abstract base class refactor

--- tests/unittests/player_service_tests/test_create_player.py
+++ tests/unittests/player_service_tests/test_create_player.py
@@ -3,7 +3,6 @@ import unittest
 from unittest.mock import patch, MagicMock
 
 import django
-from django.db import connection
 
 from core.common.error_messages import ErrorMessage
 
@@ -19,20 +18,13 @@ from core.data_access.models.player_model import Player
 from core.serializers.player_serializer import PlayerSerializer
 from core.services.player_service import PlayerService
 
-
 class TestPlayerService(TestCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        # Disable foreign key checks
-        with connection.cursor() as cursor:
-            cursor.execute('PRAGMA foreign_keys = OFF;')
         # Ensure migrations are applied before tests
+        call_command('makemigrations')
         call_command('migrate')
-        # Re-enable foreign key checks
-        with connection.cursor() as cursor:
-            cursor.execute('PRAGMA foreign_keys = ON;')
 
     @patch('core.data_access.repositories.player_repository.PlayerRepository.email_exists')
     @patch('core.serializers.player_serializer.PlayerSerializer.save')
@@ -77,29 +69,19 @@ class TestPlayerService(TestCase):
         self.assertEqual(player.firstname, 'testFirst')
         self.assertEqual(player.lastname, 'testLast')
 
-    @patch('core.data_access.repositories.player_repository.PlayerRepository.email_exists')
-    @patch('core.serializers.player_serializer.PlayerSerializer.save')
-    @patch('core.serializers.player_serializer.PlayerSerializer.is_valid')
-    def test_create_player_invalid_data(self, mock_is_valid, mock_save, mock_email_exists):
-        # Mock the email_exists to simulate email does not exist
-        mock_email_exists.return_value = False
-        mock_is_valid.return_value = False
-        mock_errors = {'email': ['This field is required.']}
-        mock_is_valid.errors = mock_errors
 
-        # Call the service method
+    def test_create_player_invalid_data(self):
         data = {
             'firstname': 'testFirstName',
             'lastname': 'testLastName',
-            'email': 'test@example.com'
+            'email': 'testexample.com'
         }
         result = PlayerService().create_player(data)
 
+        expected_error = ErrorMessage.must_contain_at_symbol(data['email'], '@')
         # Assertions
         self.assertFalse(result.is_success)
-        self.assertEqual(result.error, mock_errors)
-        mock_email_exists.assert_called_once_with(data['email'])
-        mock_save.assert_not_called()
+        self.assertEqual(result.error, expected_error)
 
 
 if __name__ == '__main__':
