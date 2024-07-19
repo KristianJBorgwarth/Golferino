4283f4441453ddca7743b9ea7df421d1ad2f8f83
Kristian <fich2402@gmail.com>
2024-07-19 10:56:13 +0200

    feat: added query, handler and serializer

---
+++ core/serializers/location/get_locations_query_serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from core.common.validators import validate_max_length, validate_min_length, validate_non_empty
+
+
+
+class GetLocationsQuerySerializer(serializers.Serializer):
+    page = serializers.IntegerField(validators=[lambda value: validate_non_empty, 
+                                                lambda value: validate_min_length(value, 1)], default=1)
+    page = serializers.IntegerField(validators=[lambda value: validate_non_empty, 
+                                                lambda value: validate_min_length(value,1),
+                                                lambda value: validate_max_length(10)], default=1)
