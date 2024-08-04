e42b78a26962d6c5305685c9f519144c6f3cbec3
Kristian <89026749+KristianJBorgwarth@users.noreply.github.com>
2024-08-04 18:18:03 +0200

    Logging (#19)
    * feat: added docker-compose file containing postgres and seq-container
    
    * feat: loggin to seqlog added
    
    * feat: added logging in GetLocationsQueryHandler

--- core/queries/location/get/get_locations_query_handler.py
+++ core/queries/location/get/get_locations_query_handler.py
@@ -1,4 +1,6 @@
 
+import logging
+import django
 from typing import List
 from core.common.mediator import RequestHandler
 from core.common.results import Result
@@ -9,26 +11,31 @@ from core.dtos.location_dto import LocationDto
 from core.queries.location.get.get_locations_query import GetLocationsQuery
 from core.serializers.location.get_locations_query_serializer import GetLocationsQuerySerializer
 
-
 class GetLocationsQueryHandler(RequestHandler[GetLocationsQuery, Result[List[LocationDto]]]):
     def __init__(self):
         self.location_repository = LocationRepository(Location)
+        self.logger = logging.getLogger(__name__)
+
         
     def handle(self, query: GetLocationsQuery) -> Result[List[LocationDto]]:
-        serializer = GetLocationsQuerySerializer(data={
-            'page': query.page,
-            'page_size': query.page_size
-        })
-        if not serializer.is_valid():
-            return Result.fail(error=serializer.errors, status_code=400)
+        try:
+            serializer = GetLocationsQuerySerializer(data={
+                'page': query.page,
+                'page_size': query.page_size
+            })
+            if not serializer.is_valid():
+                return Result.fail(error=serializer.errors, status_code=400)
+            locations = self.location_repository.get_all()
         
-        locations = self.location_repository.get_all()
+            if not locations:
+                return Result.ok([], 204)
         
-        if not locations:
-            return Result.ok([], 204)
+            paginator = Paginator(locations, query.page_size)
+            paged_locations = paginator.get_page(query.page)
+            paged_locationDtos = LocationDto(paged_locations, many=True).data
         
-        paginator = Paginator(locations, query.page_size)
-        paged_locations = paginator.get_page(query.page)
-        paged_locationDtos = LocationDto(paged_locations, many=True).data
+            return Result.ok(paged_locationDtos, 200)
         
-        return Result.ok(paged_locationDtos, 200)
+        except Exception as e:
+            self.logger.error("An error occurred while handling the query: %s", str(e), exc_info=True)
+            return Result.fail(error="An unexpected error occured", status_code=500)
