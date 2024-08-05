9fb39c2b0c2be821f1472a6fa0cd53c316fdc1fd
Lasse Schwenger <lasse.m.schwenger@gmail.com>
2024-08-05 22:58:48 +0200

    Fixed locationid type to "PrimaryKeyRelatedField" in GolfcourseDto

--- core/dtos/golfcourse_dto.py
+++ core/dtos/golfcourse_dto.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 
+from core.data_access.models.location_model import Location
+
 
 class GolfcourseDto(serializers.Serializer):
-    locationid = serializers.IntegerField()
+    locationid = serializers.PrimaryKeyRelatedField(queryset=Location.objects.all())
     numholes = serializers.IntegerField()
     name = serializers.CharField(max_length=100)
