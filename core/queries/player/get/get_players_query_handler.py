bcb8d5a5b5638dee07ef270c39e8bec642d3ab2f
Kristian <fich2402@gmail.com>
2024-08-10 20:25:27 +0200

    ref: added logging to all handlers

--- core/queries/player/get/get_players_query_handler.py
+++ core/queries/player/get/get_players_query_handler.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List
 
 from django.core.paginator import Paginator
@@ -8,21 +9,26 @@ from core.data_access.models.player_model import Player
 from core.data_access.repositories.player_repository import PlayerRepository
 from core.dtos.player_dto import PlayerDto
 from core.queries.player.get.get_players_query import GetPlayersQuery
-from core.serializers.player.get_players_query_serializer import GetPlayersQuerySerializer
 
 
 class GetPlayersQueryHandler(RequestHandler[GetPlayersQuery, Result[List[PlayerDto]]]):
     def __init__(self):
         self.player_repository = PlayerRepository(Player)
+        self.logger = logging.getLogger(__name__)
 
     def handle(self, query: GetPlayersQuery) -> Result[List[PlayerDto]]:
-        players = self.player_repository.get_all()
+        try:
+            
+            players = self.player_repository.get_all()
 
-        if not players:
-            return Result.ok([], 204)
+            if not players:
+                return Result.ok([], 204)
 
-        paginator = Paginator(players, query.page_size)
-        paged_players = paginator.get_page(query.page)
-        paged_playerDtos = PlayerDto(paged_players, many=True).data
+            paginator = Paginator(players, query.page_size)
+            paged_players = paginator.get_page(query.page)
+            paged_playerDtos = PlayerDto(paged_players, many=True).data
 
-        return Result.ok(paged_playerDtos, 200)
+            return Result.ok(paged_playerDtos, 200)
+        except Exception as e:
+            self.logger.error("An error occurred while handling the query: %s", str(e), exc_info=True)
+            return Result.fail(error="An unexpected error occured", status_code=500)
