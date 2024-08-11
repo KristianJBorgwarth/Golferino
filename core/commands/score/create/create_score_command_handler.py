f23c92d5142d1f3c29fe167b0a1f18fe2c13b6c6
Lasse Schwenger <lasse.m.schwenger@gmail.com>
2024-08-11 17:11:49 +0200

    added try except and logger to Score Handler

--- core/commands/score/create/create_score_command_handler.py
+++ core/commands/score/create/create_score_command_handler.py
@@ -1,17 +1,14 @@
-from core.commands.golfhole.create.create_golfhole_command import CreateGolfholeCommand
+import logging
+
 from core.commands.score.create.create_score_command import CreateScoreCommand
 from core.common.error_messages import ErrorMessage
 from core.common.results import Result
-from core.data_access.models.golfcourse_model import Golfcourse
 from core.data_access.models.playerround_model import Playerround
 from core.data_access.models.score_model import Score
-from core.data_access.repositories.golfcourse_repository import GolfcourseRepository
 from core.common.mediator import RequestHandler
-from core.data_access.repositories.golfhole_repository import GolfholeRepository
 from core.data_access.repositories.playerround_repository import PlayerroundRepository
 from core.data_access.repositories.score_repository import ScoreRepository
-from core.dtos.golfhole_dto import GolfholeDto
-from core.data_access.models.golfhole_model import Golfhole
+
 from core.dtos.score_dto import ScoreDto
 
 
@@ -19,20 +16,24 @@ class CreateScoreCommandHandler(RequestHandler[CreateScoreCommand, Result[ScoreD
     def __init__(self):
         self.playerround_repository = PlayerroundRepository(Playerround)
         self.score_repository = ScoreRepository(Score)
+        self.logger = logging.getLogger(__name__)
 
     def handle(self, command: CreateScoreCommand) -> Result[ScoreDto]:
-        score = Score(None, command.playerroundid, command.golfholeid, command.strokes)
+        try:
+            score = Score(None, command.playerroundid, command.golfholeid, command.strokes)
 
-        if not self.playerround_repository.playerround_exists(playerroundid=command.playerroundid):
-            print(f"{command.playerroundid} not found")
+            if not self.playerround_repository.playerround_exists(playerroundid=command.playerroundid):
+                print(f"{command.playerroundid} not found")
 
-            return Result.fail(ErrorMessage.not_found(message=command.playerroundid), status_code=400)
+                return Result.fail(ErrorMessage.not_found(message=command.playerroundid), status_code=400)
 
-        print(score.golfholeid)
-        if self.score_repository.score_exists(playerroundid=command.playerroundid, golfholeid=command.golfholeid):
-            return Result.fail(ErrorMessage.already_exists(field_name=command.golfholeid), status_code=400)
+            print(score.golfholeid)
+            if self.score_repository.score_exists(playerroundid=command.playerroundid, golfholeid=command.golfholeid):
+                return Result.fail(ErrorMessage.already_exists(field_name=command.golfholeid), status_code=400)
 
-        score = self.score_repository.create(score)
-        scoreDto = ScoreDto(score)
-
+            score = self.score_repository.create(score)
+            scoreDto = ScoreDto(score)
+        except Exception as e:
+            self.logger.error("An error occured while handling the command: %s", str(e), exc_info=True)
+            return Result.fail(error="An unexpected error occured", status_code=500)
         return Result.ok(scoreDto.data, status_code=200)
