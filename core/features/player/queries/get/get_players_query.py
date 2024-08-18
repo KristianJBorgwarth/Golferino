e1f5bd16e68e71a5639a796b3157c4392bbcdecc
Lasse Schwenger <lasse.m.schwenger@gmail.com>
2024-08-18 23:38:32 +0200

    refactor-player-dto

--- core/features/player/queries/get/get_players_query.py
+++ core/features/player/queries/get/get_players_query.py
@@ -1,11 +1,10 @@
 from typing import List
-from rest_framework import serializers
 
 from core.common.mediator import Request
-from core.dtos.player_dto import PlayerDto
+from core.features.player.queries.get.get_player_dto import GetPlayerDto
 
 
-class GetPlayersQuery(Request[List[PlayerDto]]):
+class GetPlayersQuery(Request[List[GetPlayerDto]]):
     def __init__(self, page: int, page_size: int):
         self.page = page
         self.page_size = page_size
