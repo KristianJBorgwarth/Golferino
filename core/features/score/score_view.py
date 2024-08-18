a24152ad846e7aae4c2a9ea79a033eaf08664799
Lasse Schwenger <lasse.m.schwenger@gmail.com>
2024-08-18 16:19:00 +0200

    added get-score for playerround

--- core/features/score/score_view.py
+++ core/features/score/score_view.py
@@ -4,6 +4,9 @@ from rest_framework import viewsets
 from core.features.score.commands.create.create_score_command import CreateScoreCommand
 from core.dtos.score_dto import ScoreDto
 from core.features.score.commands.create.create_score_cmd_serializer import CreateScoreCommandSerializer
+from core.features.score.queries.get.get_score_dto import GetScoreDto
+from core.features.score.queries.get.get_scores_query import GetScoresQuery
+from core.features.score.queries.get.get_scores_query_serializer import GetScoresQuerySerializer
 from core.setup.mediator_setup import get_mediator
 from core.common.ResponseEnvelope import ResponseEnvelope
 
@@ -23,6 +26,21 @@ class ScoreView(viewsets.ViewSet):
                                  request.data.get('strokes')
                                  )
         result = self._mediator.send(cmd)
+        if result.is_success:
+            return ResponseEnvelope.success(result.value, result.status_code)
+        else:
+            return ResponseEnvelope.fail(result.error, result.status_code)
+
+    @swagger_auto_schema(
+        query_serializer=GetScoresQuerySerializer,
+        responses={200: GetScoreDto(many=True), 204: 'No Content', 400: 'BadRequest'}
+    )
+    def get_all(self, request):
+        query = GetScoresQuery(page=int(request.query_params.get('page', 1)),
+                               page_size=int(request.query_params.get('page_size', 10)),
+                               playerroundid=int(request.query_params.get('playerroundid')))
+
+        result = self._mediator.send(query)
         if result.is_success:
             return ResponseEnvelope.success(result.value, result.status_code)
         else:
