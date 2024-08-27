2a163bc395ed9f356cefab2ce332b528438a3088
Lasse Schwenger <lasse.m.schwenger@gmail.com>
2024-08-27 21:04:22 +0200

    added golfhole id to dto, and locationid to dto.

--- core/features/location/commands/create/create_location_dto.py
+++ core/features/location/commands/create/create_location_dto.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 
 class CreateLocationDto(serializers.Serializer):
+    locationid = serializers.IntegerField()
     locationname = serializers.CharField(max_length=255)
     address = serializers.CharField(max_length=1024)
     city = serializers.CharField(max_length=255)
